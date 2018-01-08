6decff410538739b8b4c697149607b788917b9a5
Gary Divine <gary.divine@libertymutual.com>
2018-01-08 09:57:34 -0500

    initial commit

---
+++ src/main/java/com/lmig/gfc/cookiejarbe/CookieJarBeApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.cookiejarbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieJarBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieJarBeApplication.class, args);
+	}
+}
