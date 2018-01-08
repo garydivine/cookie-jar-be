c2f86c9feec8aba7eb68268d048fe25503ba3525
jon.graca@gmail.com <jon.graca@gmail.com>
2018-01-08 16:31:26 -0500

    Updating with IngredientRecipeApiController and associated Repo

--- src/main/java/com/lmig/gfc/cookiejarbe/models/IngredientRecipeListItem.java
+++ src/main/java/com/lmig/gfc/cookiejarbe/models/IngredientRecipeListItem.java
@@ -7,6 +7,8 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 @Entity
 public class IngredientRecipeListItem {
 
@@ -19,7 +21,7 @@ public class IngredientRecipeListItem {
 	private String unitOfMeasurement;
 
 	@ManyToOne()
-	// @JsonIgnore
+	@JsonIgnore
 	private Recipe recipe;
 
 	@OneToOne
